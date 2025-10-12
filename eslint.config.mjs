62c95f04a864f2d5f49fc8078014a9dc3f32cff0
Pedro Araújo <pdanielaraujo21@gmail.com>
2025-10-12 16:14:31 +0100

    initial prisma setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,46 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import { globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default tseslint.config([
+  globalIgnores(["dist", "node_modules/**", "eslint.config.*"]),
+
   {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      // Base JS recommendations
+      eslint.configs.recommended,
+      // TypeScript: strict + stylistic (type-aware variants)
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+      eslintPluginPrettierRecommended,
+    ],
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      sourceType: 'commonjs',
+      sourceType: "commonjs",
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  {
+
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      "@typescript-eslint/no-extraneous-class": [
+        "error",
+        {
+          allowEmpty: true,
+        },
+      ],
+      // '@typescript-eslint/no-confusing-void-expression': 'off',
+      // '@typescript-eslint/consistent-type-definitions': 'off',
+      // '@typescript-eslint/no-explicit-any': 'off',
+      // '@typescript-eslint/no-floating-promises': 'warn',
+      // '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
-);
+]);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+bootstrap()
+  .then(() => {
+    console.log("Application is running...");
+  })
+  .catch((err: unknown) => {
+    console.error("Error during application bootstrap:", err);
+  });
